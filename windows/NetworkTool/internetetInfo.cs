c929c0e4664b8464f606c69bb59dc851aab12b89
prakashnarayanshukla <prakashnarayan.shukla@servicemax.com>
2023-01-13 14:33:10 +0530

    mobile data

---
+++ windows/NetworkTool/internetetInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace NetworkTool
+{
+    internal class internetetInfo
+    {
+       public string isInternetConnected;
+       public string isWifi;
+       public string connectCost;
+
+      public internetetInfo(string isInternetConnected,string isWifi,string connectCost)
+        {
+            this.isInternetConnected = isInternetConnected;
+            this.isWifi = isWifi;
+            this.connectCost = connectCost;
+        }
+
+    }
+}
--- windows/NetworkTool/netInfo.cs
+++ windows/NetworkTool/netInfo.cs
@@ -5,20 +5,46 @@ using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.ReactNative.Managed;
+using Windows.Networking.Connectivity;
+
 namespace NetworkTool
 {
     [ReactModule]
     public class NetInfo
     {
         [ReactMethod("getNetInfo")]
-        public bool GetNetInfo()
+        public string GetNetInfo()
         {
             bool isInternetConnected = NetworkInterface.GetIsNetworkAvailable();
-            AddEvent(isInternetConnected);
-            return isInternetConnected;
+            ConnectionProfile InternetConnectionProfile = NetworkInformation.GetInternetConnectionProfile();
+            bool isWLANConnection = (InternetConnectionProfile == null) ? false : InternetConnectionProfile.IsWlanConnectionProfile;
+
+
+            string wifi = "" + isWLANConnection;
+            string isInternet = "" + isInternetConnected;
+
+
+            bool ismobile = (InternetConnectionProfile == null) ? false : InternetConnectionProfile.IsWwanConnectionProfile;
+            string mobile = "" + ismobile;
+
+            var connectionCost = NetworkInformation.GetInternetConnectionProfile().GetConnectionCost();
+            string connection = "" + connectionCost.NetworkCostType;
+            internetetInfo internetetInfo = new internetetInfo(isInternet, wifi, connection);
+
+            if (connectionCost.NetworkCostType == NetworkCostType.Unknown
+        || connectionCost.NetworkCostType == NetworkCostType.Unrestricted) {
+                //Connection cost is unknown/unrestricted
+            }
+            else {
+                //Metered Network
+            }
+
+
+            AddEvent(internetetInfo);
+            return "\n     "+internetetInfo.isInternetConnected+ "\n     " + internetetInfo.isWifi+ "\n     " + internetetInfo.connectCost;
         }
 
         [ReactEvent]
-        public Action<bool> AddEvent { get; set; }
+        public Action<object> AddEvent { get; set; }
     }
 }
